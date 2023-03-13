33ceeb42731e0e7df269dc91c23f6c4f7936ab0d
NotIntense <mbro8210@gmail.com>
2023-03-12 20:33:35 -0400

    Added lists and a randomization

--- CustomDeathMessages/Config.cs
+++ CustomDeathMessages/Config.cs
@@ -1,4 +1,5 @@
 ﻿using Exiled.API.Interfaces;
+using System.Collections.Generic;
 using System.ComponentModel;
 
 namespace CustomDeathMessages
@@ -10,57 +11,58 @@ namespace CustomDeathMessages
 
         [Description("No need to touch this :3")]
         public bool Debug { get; set; } = false;
-
+        
         [Description("The death reason for Class-D personnel.")]
-        public string ClassDdeathreason { get; set; } = "Unset";
+        public List<string> ClassDdeathreason { get; set; } = new List<string> { "Unset" };
 
         [Description("The death reason for Chaos Conscripts.")]
-        public string ChaosConscriptDeathReason { get; set; } = "Unset";
+        public List<string> ChaosConscriptDeathReason { get; set; } = new List<string> { "Unset" };
 
         [Description("The death reason for Chaos Marauders.")]
-        public string ChaosMarauderDeathReason { get; set; } = "Unset";
+        public List<string> ChaosMarauderDeathReason { get; set; } = new List<string> { "Unset" };
 
         [Description("The death reason for Chaos Repressors.")]
-        public string ChaosRepressorDeathReason { get; set; } = "Unset";
+        public List<string> ChaosRepressorDeathReason { get; set; } = new List<string> { "Unset" };
 
         [Description("The death reason for Facility Guards.")]
-        public string FacilityGuardDeathReason { get; set; } = "Unset";
+        public List<string> FacilityGuardDeathReason { get; set; } = new List<string> { "Unset" };
 
         [Description("The death reason for NTF Captains.")]
-        public string NtfCaptainDeathReason { get; set; } = "Unset";
+        public List<string> NtfCaptainDeathReason { get; set; } = new List<string> { "Unset" };
 
         [Description("The death reason for NTF Privates.")]
-        public string NtfPrivateDeathReason { get; set; } = "Unset";
+        public List<string> NtfPrivateDeathReason { get; set; } = new List<string> { "Unset" };
 
         [Description("The death reason for NTF Sergeants.")]
-        public string NtfSergeantDeathReason { get; set; } = "Unset";
+        public List<string> NtfSergeantDeathReason { get; set; } = new List<string> { "Unset" };
 
         [Description("The death reason for NTF Specialists.")]
-        public string NtfSpecialistDeathReason { get; set; } = "Unset";
+        public List<string> NtfSpecialistDeathReason { get; set; } = new List<string> { "Unset" };
 
         [Description("The death reason for Scientists.")]
-        public string ScientistDeathReason { get; set; } = "Unset";
+        public List<string> ScientistDeathReason { get; set; } = new List<string> { "Unset" };
 
         [Description("The death reason for SCP-049.")]
-        public string Scp049DeathReason { get; set; } = "Unset";
+        public List<string> Scp049DeathReason { get; set; } = new List<string> { "Unset" };
 
         [Description("The death reason for SCP-049-2.")]
-        public string Scp0492DeathReason { get; set; } = "Unset";
+        public List<string> Scp0492DeathReason { get; set; } = new List<string> { "Unset" };
 
         [Description("The death reason for SCP-079.")]
-        public string Scp079DeathReason { get; set; } = "DONT SET | DOSENT WORK";
+        public List<string> Scp079DeathReason { get; set; } = new List<string> { "Dont touch this please" };
 
         [Description("The death reason for SCP-096.")]
-        public string Scp096DeathReason { get; set; } = "Unset";
+        public List<string> Scp096DeathReason { get; set; } = new List<string> { "Unset" };
 
         [Description("The death reason for SCP-106.")]
-        public string Scp106DeathReason { get; set; } = "Unset";
+        public List<string> Scp106DeathReason { get; set; } = new List<string> { "Unset" };
 
         [Description("The death reason for SCP-173.")]
-        public string Scp173DeathReason { get; set; } = "Unset";
+        public List<string> Scp173DeathReason { get; set; } = new List<string> { "Unset" };
 
         [Description("The death reason for SCP-939.")]
-        public string Scp939DeathReason { get; set; } = "Unset";
+        public List<string> Scp939DeathReason { get; set; } = new List<string> { "Unset" };
+
 
     }
 }
--- CustomDeathMessages/Handlers/Player.cs
+++ CustomDeathMessages/Handlers/Player.cs
@@ -4,6 +4,8 @@ using Exiled.API.Features;
 using PlayerStatsSystem;
 using Exiled.API.Features.DamageHandlers;
 using UnityEngine;
+using System;
+
 
 namespace CustomDeathMessages.Handlers
 {
@@ -11,74 +13,81 @@ namespace CustomDeathMessages.Handlers
     {
         public void PlayerDied(SpawningRagdollEventArgs ev)
         {
+            System.Random random = new System.Random();
+            string deathReason;
+
             switch (ev.Player.Role.Type)
             {
                 case RoleTypeId.ClassD:
+                    deathReason = Plugin.Instance.Config.ClassDdeathreason[random.Next(Plugin.Instance.Config.ClassDdeathreason.Count)];
+                    Ragdoll.CreateAndSpawn(roleType: RoleTypeId.ClassD, ev.Player.Nickname, deathReason: deathReason, position: ev.Position, rotation: ev.Rotation);
                     ev.IsAllowed = false;
-                    Ragdoll.CreateAndSpawn(roleType: RoleTypeId.ClassD, ev.Player.Nickname, deathReason: $"{Plugin.Instance.Config.ClassDdeathreason}", position: ev.Position, rotation: ev.Rotation);
                     break;
                 case RoleTypeId.ChaosConscript:
+                    deathReason = Plugin.Instance.Config.ChaosConscriptDeathReason[random.Next(Plugin.Instance.Config.ChaosConscriptDeathReason.Count)];
+                    Ragdoll.CreateAndSpawn(roleType: RoleTypeId.ChaosConscript, ev.Player.Nickname, deathReason: deathReason, position: ev.Position, rotation: ev.Rotation);
                     ev.IsAllowed = false;
-                    Ragdoll.CreateAndSpawn(roleType: RoleTypeId.ChaosConscript, ev.Player.Nickname, deathReason: $"{Plugin.Instance.Config.ChaosConscriptDeathReason}", position: ev.Position, rotation: ev.Rotation);
                     break;
                 case RoleTypeId.ChaosMarauder:
+                    deathReason = Plugin.Instance.Config.ChaosMarauderDeathReason[random.Next(Plugin.Instance.Config.ChaosMarauderDeathReason.Count)];
+                    Ragdoll.CreateAndSpawn(roleType: RoleTypeId.ChaosMarauder, ev.Player.Nickname, deathReason: deathReason, position: ev.Position, rotation: ev.Rotation);
                     ev.IsAllowed = false;
-                    Ragdoll.CreateAndSpawn(roleType: RoleTypeId.ChaosMarauder, ev.Player.Nickname, deathReason: $"{Plugin.Instance.Config.ChaosMarauderDeathReason}", position: ev.Position, rotation: ev.Rotation);
                     break;
                 case RoleTypeId.ChaosRepressor:
+                    deathReason = Plugin.Instance.Config.ChaosRepressorDeathReason[random.Next(Plugin.Instance.Config.ChaosRepressorDeathReason.Count)];
+                    Ragdoll.CreateAndSpawn(roleType: RoleTypeId.ChaosRepressor, ev.Player.Nickname, deathReason: deathReason, position: ev.Position, rotation: ev.Rotation);
                     ev.IsAllowed = false;
-                    Ragdoll.CreateAndSpawn(roleType: RoleTypeId.ChaosRepressor, ev.Player.Nickname, deathReason: $"{Plugin.Instance.Config.ChaosRepressorDeathReason}", position: ev.Position, rotation: ev.Rotation);
                     break;
                 case RoleTypeId.FacilityGuard:
+                    deathReason = Plugin.Instance.Config.FacilityGuardDeathReason[random.Next(Plugin.Instance.Config.FacilityGuardDeathReason.Count)];
+                    Ragdoll.CreateAndSpawn(roleType: RoleTypeId.FacilityGuard, ev.Player.Nickname, deathReason: deathReason, position: ev.Position, rotation: ev.Rotation);
                     ev.IsAllowed = false;
-                    Ragdoll.CreateAndSpawn(roleType: RoleTypeId.FacilityGuard, ev.Player.Nickname, deathReason: $"{Plugin.Instance.Config.FacilityGuardDeathReason}", position: ev.Position, rotation: ev.Rotation);
                     break;
                 case RoleTypeId.NtfCaptain:
+                    deathReason = Plugin.Instance.Config.NtfCaptainDeathReason[random.Next(Plugin.Instance.Config.NtfCaptainDeathReason.Count)];
+                    Ragdoll.CreateAndSpawn(roleType: RoleTypeId.NtfCaptain, ev.Player.Nickname, deathReason: deathReason, position: ev.Position, rotation: ev.Rotation);
                     ev.IsAllowed = false;
-                    Ragdoll.CreateAndSpawn(roleType: RoleTypeId.NtfCaptain, ev.Player.Nickname, deathReason: $"{Plugin.Instance.Config.NtfCaptainDeathReason}", position: ev.Position, rotation: ev.Rotation);
                     break;
                 case RoleTypeId.NtfPrivate:
+                    deathReason = Plugin.Instance.Config.NtfPrivateDeathReason[random.Next(Plugin.Instance.Config.NtfPrivateDeathReason.Count)];
+                    Ragdoll.CreateAndSpawn(roleType: RoleTypeId.NtfPrivate, ev.Player.Nickname, deathReason: deathReason, position: ev.Position, rotation: ev.Rotation);
                     ev.IsAllowed = false;
-                    Ragdoll.CreateAndSpawn(roleType: RoleTypeId.NtfPrivate, ev.Player.Nickname, deathReason: $"{Plugin.Instance.Config.NtfPrivateDeathReason}", position: ev.Position, rotation: ev.Rotation);
                     break;
                 case RoleTypeId.NtfSergeant:
+                    deathReason = Plugin.Instance.Config.NtfSergeantDeathReason[random.Next(Plugin.Instance.Config.NtfSergeantDeathReason.Count)];
+                    Ragdoll.CreateAndSpawn(roleType: RoleTypeId.NtfSergeant, ev.Player.Nickname, deathReason: deathReason, position: ev.Position, rotation: ev.Rotation);
                     ev.IsAllowed = false;
-                    Ragdoll.CreateAndSpawn(roleType: RoleTypeId.NtfSergeant, ev.Player.Nickname, deathReason: $"{Plugin.Instance.Config.NtfSergeantDeathReason}", position: ev.Position, rotation: ev.Rotation);
                     break;
                 case RoleTypeId.NtfSpecialist:
-                    ev.IsAllowed = false;
-                    Ragdoll.CreateAndSpawn(roleType: RoleTypeId.NtfSpecialist, ev.Player.Nickname, deathReason: $"{Plugin.Instance.Config.NtfSpecialistDeathReason}", position: ev.Position, rotation: ev.Rotation);
-                    break;
-                case RoleTypeId.Scientist:
-                    ev.IsAllowed = false;
-                    Ragdoll.CreateAndSpawn(roleType: RoleTypeId.Scientist, ev.Player.Nickname, deathReason: $"{Plugin.Instance.Config.ScientistDeathReason}", position: ev.Position, rotation: ev.Rotation);
+                    deathReason = Plugin.Instance.Config.NtfSpecialistDeathReason[random.Next(Plugin.Instance.Config.NtfSpecialistDeathReason.Count)];
+                    Ragdoll.CreateAndSpawn(roleType: RoleTypeId.NtfSpecialist, ev.Player.Nickname, deathReason: deathReason, position: ev.Position, rotation: ev.Rotation);
                     break;
                 case RoleTypeId.Scp049:
-                    ev.IsAllowed = false;
-                    Ragdoll.CreateAndSpawn(roleType: RoleTypeId.Scp049, ev.Player.Nickname, deathReason: $"{Plugin.Instance.Config.Scp049DeathReason}", position: ev.Position, rotation: ev.Rotation);
+                    deathReason = Plugin.Instance.Config.NtfSpecialistDeathReason[random.Next(Plugin.Instance.Config.Scp049DeathReason.Count)];
+                    Ragdoll.CreateAndSpawn(roleType: RoleTypeId.NtfSpecialist, ev.Player.Nickname, deathReason: deathReason, position: ev.Position, rotation: ev.Rotation);
                     break;
                 case RoleTypeId.Scp0492:
-                    break;
-                    Ragdoll.CreateAndSpawn(roleType: RoleTypeId.Scp0492, ev.Player.Nickname, deathReason: $"{Plugin.Instance.Config.Scp0492DeathReason}", position: ev.Position, rotation: ev.Rotation);
-                case RoleTypeId.Scp079:
+                    deathReason = Plugin.Instance.Config.NtfSpecialistDeathReason[random.Next(Plugin.Instance.Config.Scp0492DeathReason.Count)];
+                    Ragdoll.CreateAndSpawn(roleType: RoleTypeId.NtfSpecialist, ev.Player.Nickname, deathReason: deathReason, position: ev.Position, rotation: ev.Rotation);
                     break;
                 case RoleTypeId.Scp096:
-                    ev.IsAllowed = false;
-                    Ragdoll.CreateAndSpawn(roleType: RoleTypeId.Scp096, ev.Player.Nickname, deathReason: $"{Plugin.Instance.Config.Scp096DeathReason}", position: ev.Position, rotation: ev.Rotation);
+                    deathReason = Plugin.Instance.Config.NtfSpecialistDeathReason[random.Next(Plugin.Instance.Config.Scp096DeathReason.Count)];
+                    Ragdoll.CreateAndSpawn(roleType: RoleTypeId.NtfSpecialist, ev.Player.Nickname, deathReason: deathReason, position: ev.Position, rotation: ev.Rotation);
                     break;
                 case RoleTypeId.Scp106:
-                    ev.IsAllowed = false;
-                    Ragdoll.CreateAndSpawn(roleType: RoleTypeId.Scp106, ev.Player.Nickname, deathReason: $"{Plugin.Instance.Config.Scp106DeathReason}", position: ev.Position, rotation: ev.Rotation);
+                    deathReason = Plugin.Instance.Config.NtfSpecialistDeathReason[random.Next(Plugin.Instance.Config.Scp106DeathReason.Count)];
+                    Ragdoll.CreateAndSpawn(roleType: RoleTypeId.NtfSpecialist, ev.Player.Nickname, deathReason: deathReason, position: ev.Position, rotation: ev.Rotation);
                     break;
                 case RoleTypeId.Scp173:
-                    ev.IsAllowed = false;
-                    Ragdoll.CreateAndSpawn(roleType: RoleTypeId.Scp173, ev.Player.Nickname, deathReason: $"{Plugin.Instance.Config.Scp173DeathReason}", position: ev.Position, rotation: ev.Rotation);
+                    deathReason = Plugin.Instance.Config.NtfSpecialistDeathReason[random.Next(Plugin.Instance.Config.Scp173DeathReason.Count)];
+                    Ragdoll.CreateAndSpawn(roleType: RoleTypeId.NtfSpecialist, ev.Player.Nickname, deathReason: deathReason, position: ev.Position, rotation: ev.Rotation);
                     break;
                 case RoleTypeId.Scp939:
-                    ev.IsAllowed = false;
-                    Ragdoll.CreateAndSpawn(roleType: RoleTypeId.Scp939, ev.Player.Nickname, deathReason: $"{Plugin.Instance.Config.Scp939DeathReason}", position: ev.Position, rotation: ev.Rotation);
-                    break;              
+                    deathReason = Plugin.Instance.Config.NtfSpecialistDeathReason[random.Next(Plugin.Instance.Config.Scp939DeathReason.Count)];
+                    Ragdoll.CreateAndSpawn(roleType: RoleTypeId.NtfSpecialist, ev.Player.Nickname, deathReason: deathReason, position: ev.Position, rotation: ev.Rotation);
+                    break;
             }
+
         }
     }
 }
